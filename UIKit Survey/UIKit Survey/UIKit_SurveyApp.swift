222b7638e7de2abf3ff621e1ef2cdb8b9cb3448c
Nina <nnotomi@gmail.com>
2021-10-16 19:33:42 +1100

    Initial files

---
+++ UIKit Survey/UIKit Survey/UIKit_SurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKit_SurveyApp.swift
+//  UIKit Survey
+//
+//  Created by Nina on 16/10/21.
+//
+
+import SwiftUI
+
+@main
+struct UIKit_SurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
